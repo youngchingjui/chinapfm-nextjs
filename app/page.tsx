5dc3531777d84d6a53eb9357c8a287f6ec0b7ed1
youngchingjui <young.chingjui@gmail.com>
2024-03-07 11:23:38 +0800

    added form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return (
+        <>
+            <h1>Hello, Next.js!</h1>
+            <form>
+                <input type="file" name="file" id="file" accept=".csv, .txt" />
+                <button type="submit">Upload</button>
+            </form>
+        </>
+    )
 }
