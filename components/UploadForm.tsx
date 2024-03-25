e0bc24efe4e7d2dc2ad8f5feedca3265374c1d6c
Ching Jui Young <young.chingjui@gmail.com>
2024-03-25 23:37:59 +0800

    move away from actions

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -1,19 +1,38 @@
 "use client"
 
-import { useFormState } from "react-dom"
+import { useState } from "react"
 
-import { upload } from "../lib/actions/actions"
 import SubmitButton from "./SubmitButton"
 
 export default function UploadForm() {
-    const [state, formAction] = useFormState(upload, {
-        message: "initial message state",
-    })
+    const [file, setFile] = useState<File | null>(null)
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+
+        const formData = new FormData()
+        formData.append("file", file)
+
+        const response = await fetch("/api/upload", {
+            method: "POST",
+            body: formData,
+        })
+
+        console.log("response", response)
+    }
+
     return (
-        <form action={formAction}>
-            <input type="file" name="file" id="file" accept=".csv, .txt" />
+        <form onSubmit={handleSubmit}>
+            <input
+                type="file"
+                name="file"
+                id="file"
+                accept=".csv, .txt"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setFile((e.target.files as FileList)[0] || null)
+                }
+            />
             <div>State action:</div>
-            <div>{state?.message}</div>
             <SubmitButton />
         </form>
     )
